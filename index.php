db3f7443d0f5ce27aef378d8f61d5119bb5da9c6
Ebi Baralaye <tech@baralaye.com>
2014-03-22 20:53:04 -0400

    various updates to core php functionality

--- index.php
+++ index.php
@@ -8,41 +8,9 @@ if (strpos($path, '?') !== FALSE) {
   $path = substr($path, 0, strpos($path, '?'));
 }
 
-$content = "";
+$action = route($path);
 
-if ($path == '/') {
-    $content = render('pages/home.php');
-}
-else if(substr($path, 0,4) == '/art') {
-    $products = array();
-    $products_ref = fopen("database/products.csv", "r");
-    $properties = fgetcsv($products_ref);
-    $properties = array_slice($properties, 0, 15);
-
-    while (($row = fgetcsv($products_ref)) !== FALSE){
-        $row = array_slice($row, 0, 15);
-        $product = array_combine($properties, $row);
-        if ($path == $product["url_path"]) {
-            $content = render("templates/catalog/detail.php", $product);
-            break;
-        }
-        else if(strpos($product["url_path"], $path) === 0) {
-            $products[] = $product;
-        }
-    }
-
-    fclose($products_ref);
-
-    if (count($products) > 0) {
-        $content = render("templates/catalog/list.php", array('products' => $products));
-    }
-}
-else if(file_exists("pages{$path}.php")){
-    $content = render("pages{$path}.php");
-}
-else {
-    $content = render('pages/error/404.php');
-}
+$content = call_user_func("action_".$action, $path);
 
 $mainMenu = render('templates/menus/main.php');
 
--- library/core.php
+++ library/core.php
@@ -1,5 +1,16 @@
 <?php
 
+function conf() {
+    $conf = array(
+        'database' => array (
+            'dsn' => 'mysql:dbname=dev_baralaye;host=127.0.0.1',
+            'user' => 'root',
+            'password' => 'root',
+        )
+    );
+    return $conf;
+}
+
 function render($template, $variables = array()) {
     extract($variables);
     ob_start();
@@ -7,4 +18,106 @@ function render($template, $variables = array()) {
     $content = ob_get_contents();
     ob_end_clean();
     return $content;
+}
+
+function route($path) {
+    if ($path == '/') {
+        return "home";
+    }
+    else if(file_exists("pages{$path}.php")){
+        return "page";
+    }
+    else if(substr($path, 0,4) == '/art') {
+        return "catalog";
+    }
+    else if(substr($path, 0,5) == '/news') {
+        return "news";
+    }
+    else {
+        return "404";
+    }
+}
+
+function action_home($path){
+    return render('pages/home.php');
+}
+
+function action_page($path){
+    return render("pages{$path}.php");
+}
+
+function action_catalog($path){
+    $products = array();
+
+    $products_ref = fopen("database/products.csv", "r");
+
+    $properties = fgetcsv($products_ref);
+    $properties = array_slice($properties, 0, 15);
+
+    while (($row = fgetcsv($products_ref)) !== FALSE){
+        $row = array_slice($row, 0, 15);
+        $product = array_combine($properties, $row);
+        if ($path == $product["url"]) {
+            return render("templates/catalog/detail.php", $product);
+            break;
+        }
+        else if(strpos($product["url"], $path) === 0) {
+            $products[] = $product;
+        }
+    }
+
+    fclose($products_ref);
+
+    if (count($products) > 0) {
+        return render("templates/catalog/list.php", array('products' => $products));
+    }
+    else {
+       return action_404();
+    }
+}
+
+function action_404(){
+    return render('pages/error/404.php');
+}
+
+function action_news($path){
+
+    // Create a variable to hold list of post data
+    $posts = array();
+    $path = substr($path, 6);
+
+    // Open a database connection - pointer to my database
+    $conf = conf();
+    $conf = $conf['database'];
+    try {
+        $dbh = new PDO($conf['dsn'], $conf['user'], $conf['password']);
+
+        if($path){
+            // SQL statement for url slug (detail view)
+            $sql = 'SELECT * FROM posts WHERE status = 1 AND url = ?';
+            $sth = $dbh->prepare($sql);
+            $sth->execute(array($path));
+            $post = $sth->fetch();
+
+            if ($post) {
+                return render("templates/news/detail.php", $post);
+            }
+        }
+        else {
+            // selecting all posts that are published
+            $sql = 'SELECT * FROM posts WHERE status = 1 ORDER BY published_date DESC';
+            $rows = $dbh->query($sql);
+
+            foreach ($rows as $row) {
+                $posts[] = $row;
+            }
+            return render("templates/news/list.php", array('posts' => $posts));
+        }
+
+    }
+    catch (PDOException $e) {
+    }
+
+   return action_404();
+
 }
