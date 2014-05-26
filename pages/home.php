d978550f92955830ffd5a7cfe75c6208b11fde36
Ebi Baralaye <tech@baralaye.com>
2014-05-26 05:47:55 -0400

    css and image url refactoring

--- pages/home.php
+++ pages/home.php
@@ -1,13 +1,13 @@
 <div class="page-body home">
   <ul class="portfolio">
-    <li class="sculpture">
-      <h1><a href="/art/sculpture"><span>Sculpture</span></a></h1>
+    <li>
+      <h1><a class="sculpture" href="/art/sculpture"><span>Sculpture</span></a></h1>
     </li>
-    <li class="vessels">
-      <h1><a href="/art/vessels"><span>Vessels</span></a></h1>
+    <li>
+      <h1><a class="vessels" href="/art/vessels"><span>Vessels</span></a></h1>
     </li>
-    <li class="drawings">
-      <h1><a href="/art/drawings"><span>Drawings</span></a></h1>
+    <li>
+      <h1><a class="drawings" href="/art/drawings"><span>Drawings</span></a></h1>
     </li>
   </ul>
 </div>
--- pages/statement.php
+++ pages/statement.php
@@ -1,11 +1,8 @@
 <div class="page-body statement">
-    <div class="header">
-        <h1>Statement</h1>
-    </div>
-    <img class="head-shot" alt="Ebitenyefa Baralaye" src="/Images/about/Baralaye_hs-1-abstract.jpg" />
-    <p>
-    I create art as a way to bring to reality and share the things I wish to see, understand, and enjoy. In my work I abstract, deconstruct and reinterpret structure on various levels, exploring variations of wholeness, identity and character in a ways that motivate broadened perception, value and generation of these elements. I compose divergent, naturalistic and industrial geometries and gestures to build bodies of form reflective of my own life experience and textures of awareness.</p>
-    <p>
-My aesthetic is largely focused on the qualities and dynamics of form, touching on geometric abstraction, abstract expressionism, appropriation and bricolage. My production techniques are an intuitive mixture of those traditional and contrived in ceramic handbuilding, metal fabrication, mixed-media and found object assemblage, painting and patination.
-    </p>
+  <header>
+    <h1>Statement</h1>
+    <h2><img alt="Ebitenyefa Baralaye" src="/images/about/baralaye_hs-1-abstract.jpg" /></h2>
+  </header>
+  <p>I create art as a way to bring to reality and share the things I wish to see, understand, and enjoy. In my work I abstract, deconstruct and reinterpret structure on various levels, exploring variations of wholeness, identity and character in a ways that motivate broadened perception, value and generation of these elements. I compose divergent, naturalistic and industrial geometries and gestures to build bodies of form reflective of my own life experience and textures of awareness.</p>
+  <p>My aesthetic is largely focused on the qualities and dynamics of form, touching on geometric abstraction, abstract expressionism, appropriation and bricolage. My production techniques are an intuitive mixture of those traditional and contrived in ceramic handbuilding, metal fabrication, mixed-media and found object assemblage, painting and patination.</p>
 </div>
