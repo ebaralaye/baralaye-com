dfed41b5a0bcde784cf28a957661d0b68fb326ac
Ebi Baralaye <tech@baralaye.com>
2014-05-26 16:03:16 -0400

    further responsive styles

--- templates/catalog/detail.php
+++ templates/catalog/detail.php
@@ -1,46 +1,45 @@
 <div class="product-detail">
-    <div class="details">
-        <h1 class="name"><?php echo $name; ?></h1>
+  <div class="images">
+    <h1 class="name"><?php echo $name; ?></h1>
+    <div class="main">
+      <a href="/images/art/portfolio/big/<?php echo $image; ?>.jpg" class="cloud-zoom" id="zoom1" rel="position: 'inside'"><img src="/images/art/portfolio/large/<?php echo $image; ?>.jpg" alt="<?php echo $name; ?>" /></a>
+    </div>
+    <ul class="poplets">
+      <?php $poplets = explode(',', $image_poplets); ?>
+      <?php foreach($poplets as $poplet): ?>
+        <li><a href="/images/art/portfolio/big/<?php echo $poplet ?>.jpg" rel="useZoom: 'zoom1', smallImage: '/images/art/portfolio/large/<?php echo $poplet ?>.jpg'" class="cloud-zoom-gallery"><img src="/images/art/portfolio/small/<?php echo $poplet ?>.jpg" alt="<?php echo $poplet ?>" /></a></li>
+      <?php endforeach; ?>
+    </ul>
+  </div>
+  <div class="details">
+    <h1 class="name"><?php echo $name; ?></h1>
+    <ul>
+      <li class="specs">
         <ul>
-            <li class="specs">
-                <ul>
-                    <li class="medium">Medium: <?php echo $medium; ?></li>
-                    <li class="dimensions">Dimensions (inches): <?php echo $dim_width," x ", $dim_height," x ",$dim_depth; ?></li>
-                    <li class="weight">Weight: <span><?php echo $weight; ?></span> lbs</li>
-                    <li class="year">Year: <?php echo $year; ?></li>
-                    <li class="edition">Edition: <span><?php echo $edition; ?></span></li>
-                    <li class="price">Price: $<?php echo $price; ?></a></li>
-                    <li class="code">Code: <span><?php echo $code; ?></span></li>
-                </ul>
-            </li>
-            <li class="social">
-                <ul class="social-btns">
-                    <li class="btn-s purchase"><a href="#purchase-inquiry" class="ltbx win">Buy</a></li>
-                    <li class="btn-s comment"><a href="#comment-form" class="ltbx win">Comment</a></li>
-                    <li class="pin"><a href="//www.pinterest.com/pin/create/button/?url=http://baralaye.com{tag_itemurl_nolink}&media=http://baralaye.com{tag_largeimage_path}&description=Ebitenyefa%20Baralaye%20-%20{tag_title}" data-pin-do="buttonPin" data-pin-config="none"><img src="//assets.pinterest.com/images/pidgets/pin_it_button.png" /></a></li>
-                    <li class="tweet"><a href="http://twitter.com/share" class="twitter-share-button" data-count="none" data-via="baralaye">Tweet</a>
-                    <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
-                    </li>
-                    <li class="like last">
-                        <script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script>
-                        <fb:like send="false" layout="button_count" width="75" show_faces="false"></fb:like>
-                    </li>
-                    <li class="c-z"></li>
-                </ul>
-            </li>
-            <li class="description"><?php echo $description; ?></li>
+          <li class="medium">Medium: <?php echo $medium; ?></li>
+          <li class="dimensions">Dimensions (inches): <?php echo $dim_width," x ", $dim_height," x ",$dim_depth; ?></li>
+          <li class="weight">Weight: <span><?php echo $weight; ?></span> lbs</li>
+          <li class="year">Year: <?php echo $year; ?></li>
+          <li class="edition">Edition: <span><?php echo $edition; ?></span></li>
+          <li class="price">Price: $<?php echo $price; ?></a></li>
+          <li class="code">Code: <span><?php echo $code; ?></span></li>
         </ul>
-    </div>
-    <div class="images">
-        <div class="main">
-            <a href="/images/art/portfolio/big/<?php echo $image; ?>.jpg" class="cloud-zoom" id="zoom1" rel="position: 'inside'"><img src="/images/art/portfolio/large/<?php echo $image; ?>.jpg" alt="<?php echo $name; ?>" /></a>
-        </div>
-        <div class="c-z"></div>
-        <ul class="poplets">
-            <?php $poplets = explode(',', $image_poplets); ?>
-            <?php foreach($poplets as $poplet): ?>
-                <li><a href="/images/art/portfolio/big/<?php echo $poplet ?>.jpg" rel="useZoom: 'zoom1', smallImage: '/images/art/portfolio/large/<?php echo $poplet ?>.jpg'" class="cloud-zoom-gallery"><img src="/images/art/portfolio/small/<?php echo $poplet ?>.jpg" alt="<?php echo $poplet ?>" /></a></li>
-            <?php endforeach; ?>
+      </li>
+      <li class="social">
+        <ul class="social-btns">
+          <li class="btn-s purchase"><a href="#purchase-inquiry" class="ltbx win">Buy</a></li>
+          <li class="btn-s comment"><a href="#comment-form" class="ltbx win">Comment</a></li>
+          <li class="pin"><a href="//www.pinterest.com/pin/create/button/?url=http://baralaye.com{tag_itemurl_nolink}&media=http://baralaye.com{tag_largeimage_path}&description=Ebitenyefa%20Baralaye%20-%20{tag_title}" data-pin-do="buttonPin" data-pin-config="none"><img src="//assets.pinterest.com/images/pidgets/pin_it_button.png" /></a></li>
+          <li class="tweet"><a href="http://twitter.com/share" class="twitter-share-button" data-count="none" data-via="baralaye">Tweet</a>
+          <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+          </li>
+          <li class="like last">
+            <script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script>
+            <fb:like send="false" layout="button_count" width="75" show_faces="false"></fb:like>
+          </li>
         </ul>
-    </div>
+      </li>
+      <li class="description"><?php echo $description; ?></li>
+    </ul>
+  </div>
 </div>
--- templates/main.php
+++ templates/main.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Ebitenyefa Baralaye - {module_pagename}</title>
+    <title>Ebitenyefa Baralaye</title>
 
     <!-- Meta Data: -->
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="keywords" content=" sculpture, art, ceramics, vases, abstract, fine-artist, sculptor, artist" />
-    <meta name="description" content="Ebitenyefa Baralaye is a fine artist &amp; designer addressing themes of identity through abstraction." />
+    <meta name="description" content="Fine artist sculptor." />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="p:domain_verify" content="de0be4f5a7cf46b1fb6578af011e6998" />
     <meta name="robots" content="index,follow" />
