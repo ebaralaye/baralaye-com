cf7e9422f2e000cc1c5cef9e0e50068a4b542f32
Ebi Baralaye <tech@baralaye.com>
2014-05-29 22:24:08 -0400

    removed caps on in progress image file names

--- pages/in-progress.php
+++ pages/in-progress.php
@@ -8,34 +8,34 @@
       <li>
         <h4 class="name">Jacob</h4>
         <div class="images ltbx">
-          <a href="/images/art/in-progress/large/Jacob_a.jpg" rel="jacob" class="main"><img alt="Jacob" src="/images/art/in-progress/small/Jacob_a.jpg" /></a>
-          <a href="/images/art/in-progress/large/Jacob_b.jpg" rel="jacob"><img alt="Jacob" src="/images/art/in-progress/small/Jacob_b.jpg" /></a>
-          <a href="/images/art/in-progress/large/Jacob_c.jpg" rel="jacob"><img alt="Jacob" src="/images/art/in-progress/small/Jacob_c.jpg" /></a>
-          <a href="/images/art/in-progress/large/Jacob_d.jpg" rel="jacob"><img alt="Jacob" src="/images/art/in-progress/small/Jacob_d.jpg" /></a>
-          <a href="/images/art/in-progress/large/Jacob_e.jpg" rel="jacob"><img alt="Jacob" src="/images/art/in-progress/small/Jacob_e.jpg" /></a>
+          <a href="/images/art/in-progress/large/jacob_a.jpg" rel="jacob" class="main"><img alt="Jacob" src="/images/art/in-progress/small/jacob_a.jpg" /></a>
+          <a href="/images/art/in-progress/large/jacob_b.jpg" rel="jacob"><img alt="Jacob" src="/images/art/in-progress/small/jacob_b.jpg" /></a>
+          <a href="/images/art/in-progress/large/jacob_c.jpg" rel="jacob"><img alt="Jacob" src="/images/art/in-progress/small/jacob_c.jpg" /></a>
+          <a href="/images/art/in-progress/large/jacob_d.jpg" rel="jacob"><img alt="Jacob" src="/images/art/in-progress/small/jacob_d.jpg" /></a>
+          <a href="/images/art/in-progress/large/jacob_e.jpg" rel="jacob"><img alt="Jacob" src="/images/art/in-progress/small/jacob_e.jpg" /></a>
         </div>
       </li>
       <li>
         <h4 class="name">Legion</h4>
         <div class="images ltbx">
-          <a href="/images/art/in-progress/large/Legion_a.jpg" rel="legion" class="main"><img alt="Legion" src="/images/art/in-progress/small/Legion_a.jpg" /></a>
-          <a href="/images/art/in-progress/large/Legion_b.jpg" rel="legion"><img alt="Legion" src="/images/art/in-progress/small/Legion_b.jpg" /></a>
-          <a href="/images/art/in-progress/large/Legion_c.jpg" rel="legion"><img alt="Legion" src="/images/art/in-progress/small/Legion_c.jpg" /></a>
+          <a href="/images/art/in-progress/large/legion_a.jpg" rel="legion" class="main"><img alt="Legion" src="/images/art/in-progress/small/legion_a.jpg" /></a>
+          <a href="/images/art/in-progress/large/legion_b.jpg" rel="legion"><img alt="Legion" src="/images/art/in-progress/small/legion_b.jpg" /></a>
+          <a href="/images/art/in-progress/large/legion_c.jpg" rel="legion"><img alt="Legion" src="/images/art/in-progress/small/legion_c.jpg" /></a>
         </div>
       </li>
       <li>
         <h4 class="name">Untitled</h4>
         <div class="images ltbx">
-          <a href="/images/art/in-progress/large/Untitled-2_a.jpg" rel="untitled-2" class="main"><img alt="Untitled-2" src="/images/art/in-progress/small/Untitled-2_a.jpg" /></a>
-          <a href="/images/art/in-progress/large/Untitled-2_b.jpg" rel="untitled-2"><img alt="Untitled-2" src="/images/art/in-progress/small/Untitled-2_b.jpg" /></a>
+          <a href="/images/art/in-progress/large/untitled-2_a.jpg" rel="untitled-2" class="main"><img alt="untitled-2" src="/images/art/in-progress/small/untitled-2_a.jpg" /></a>
+          <a href="/images/art/in-progress/large/untitled-2_b.jpg" rel="untitled-2"><img alt="untitled-2" src="/images/art/in-progress/small/untitled-2_b.jpg" /></a>
         </div>
       </li>
       <li>
         <h4 class="name">Untitled</h4>
         <div class="images ltbx">
-          <a href="/images/art/in-progress/large/Untitled-1_a.jpg" rel="untitled-1" class="main"><img alt="Untitled-1" src="/images/art/in-progress/small/Untitled-1_a.jpg" /></a>
-          <a href="/images/art/in-progress/large/Untitled-1_b.jpg" rel="untitled-1"><img alt="Untitled-1" src="/images/art/in-progress/small/Untitled-1_b.jpg" /></a>
-          <a href="/images/art/in-progress/large/Untitled-1_c.jpg" rel="untitled-1"><img alt="Untitled-1" src="/images/art/in-progress/small/Untitled-1_c.jpg" /></a>
+          <a href="/images/art/in-progress/large/untitled-1_a.jpg" rel="untitled-1" class="main"><img alt="untitled-1" src="/images/art/in-progress/small/untitled-1_a.jpg" /></a>
+          <a href="/images/art/in-progress/large/untitled-1_b.jpg" rel="untitled-1"><img alt="untitled-1" src="/images/art/in-progress/small/untitled-1_b.jpg" /></a>
+          <a href="/images/art/in-progress/large/untitled-1_c.jpg" rel="untitled-1"><img alt="untitled-1" src="/images/art/in-progress/small/untitled-1_c.jpg" /></a>
         </div>
       </li>
     </ul>
