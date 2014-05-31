d5976f1eaab03d5eaba0a71bea77bc2eb7db4ab7
Ebi Baralaye <tech@baralaye.com>
2014-05-31 13:36:13 -0400

    news style updates

--- templates/news/detail.php
+++ templates/news/detail.php
@@ -1,19 +1,20 @@
 <div class="page-body news">
   <header>
     <h1><a href="/news">News</a></h1>
-    <p>Upcoming shows/openings, updates and other events.</p>
   </header>
   <div class="main">
     <div class="post detail">
-      <h2 class="title h-title sub"><?php echo $title; ?></h2>
-      <p class="details">posted on: <?php echo $published_date; ?></p>
-      <ul class="social-btns">
-        <li class="tweet"><a href="http://twitter.com/share" class="twitter-share-button" data-count="none" data-via="baralaye">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script></li>
-        <li class="like last"> 
-        <script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script>
-        <fb:like href="" send="false" layout="button_count" width="75" show_faces="false"></fb:like>
-        </li>
-      </ul>
+      <div class="head">
+        <h2 class="title h-title sub"><?php echo $title; ?></h2>
+        <div class="details"><time><?php $newDate = date("M-d-Y", strtotime($published_date)); echo $newDate; ?></time></div>
+        <ul class="social-btns">
+          <li class="tweet"><a href="http://twitter.com/share" class="twitter-share-button" data-count="none" data-via="baralaye">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script></li>
+          <li class="like last">
+          <script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script>
+          <fb:like href="" send="false" layout="button_count" width="75" show_faces="false"></fb:like>
+          </li>
+        </ul>
+      </div>
       <?php if ($image != null): ?>
         <div class="image">
           <img src="<?php echo $image; ?>" alt="<?php echo $image_alt; ?>">
@@ -23,11 +24,11 @@
         </div>
       <?php endif; ?>
       <div class="body"><?php echo $content; ?></div>
+      <a class="txt-btn" href="/news">+ Back</a>
     </div>
-    <div class="goBack btn-sub"><a href="/news">Go Back</a></div>
   </div>
   <div class="side">
-    <h2 class="h-title">More News...</h2>
-    <a href="/news" class="btn-sub">+ See All</a>
+    <h2 class="h-title sub"><a href="/news">Latest News</a></h2>
+    <a href="/news" class="txt-btn">All +</a>
   </div>
 </div>
--- templates/news/list.php
+++ templates/news/list.php
@@ -1,23 +1,22 @@
 <div class="page-body news">
   <header>
     <h1>News</h1>
-    <p>Upcoming shows/opennings, updates and other events.</p>
   </header>
   <div class="main">
     <ul class="post-list">
       <?php foreach ($posts as $post): ?>
         <li class="post">
           <h3 class="title h-title sub"><a href="/news/<?php echo $post['url'] ?>"><?php echo $post['title']; ?></a></h3>
-          <div class="details"><?php echo $post['published_date']; ?></div>
+          <div class="details"><time><?php $newDate = date("M-d-Y", strtotime($post['published_date'])); echo $newDate; ?></time></div>
           <div class="body">
-            <p><?php echo $post['content_blurb']; ?><a href="/news/<?php echo $post['url'] ?>" class="more">read more</a></p>
+            <p><?php echo $post['content_blurb']; ?> <a href="/news/<?php echo $post['url'] ?>" class="more">+</a></p>
           </div>
         </li>
       <?php endforeach; ?>
     </ul>
   </div>
   <div class="side">
-    <h2 class="h-title"><a href="http://twitter.com/baralaye" target="_blank">Twitter</a></h2>
+    <h3 class="h-title sub"><a href="http://twitter.com/baralaye" target="_blank">Twitter</a></h3>
     <a class="twitter-timeline" href="https://twitter.com/baralaye" data-widget-id="391761770735353857" height="700">Tweets by @baralaye</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
   </div>
