f721bcf9a5e45507027b343ec07db473cc157e2d
Ebi Baralaye <tech@baralaye.com>
2014-05-31 15:04:11 -0400

    updated 404 page

--- pages/error/404.php
+++ pages/error/404.php
@@ -1,1 +1,6 @@
-404!
+<div class="page-body error">
+  <header>
+    <h1>Hmm, Sorry. 404.</h1>
+    <h3>I can't find that page but take a look at <a href="/art">my portfolio</a> while you're here.</h3>
+  </header>
+</div>
