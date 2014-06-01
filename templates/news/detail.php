a13d1dac7b0b28c4507a6d394a4abb08cfc00db0
Ebi Baralaye <tech@baralaye.com>
2014-05-31 20:40:24 -0400

    final updates to news section?

--- templates/news/detail.php
+++ templates/news/detail.php
@@ -5,8 +5,8 @@
   <div class="main">
     <div class="post detail">
       <div class="head">
-        <h2 class="title h-title sub"><?php echo $title; ?></h2>
-        <div class="details"><time><?php $newDate = date("M-d-Y", strtotime($published_date)); echo $newDate; ?></time></div>
+        <h2 class="title h-title"><?php echo $post['title']; ?></h2>
+        <div class="details"><time><?php $newDate = date("M-d-Y", strtotime($post['published_date'])); echo $newDate; ?></time></div>
         <!--<ul class="social-btns">
           <li class="tweet"><a href="http://twitter.com/share" class="twitter-share-button" data-count="none" data-via="baralaye">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script></li>
           <li class="like last">
@@ -15,18 +15,29 @@
           </li>
         </ul>-->
       </div>
-      <?php if ($image != null): ?>
+      <?php if ($post['image'] != null): ?>
         <div class="image">
-          <img src="<?php echo $image; ?>" alt="<?php echo $image_alt; ?>">
+          <img src="<?php echo $post['image']; ?>" alt="<?php echo $post['image_alt']; ?>">
           <?php if ($image_desc != null): ?>
-            <small class="desc"><?php echo $image_desc ?></small>
+            <small class="desc"><?php echo $post['image_desc']; ?></small>
           <?php endif; ?>
         </div>
       <?php endif; ?>
-      <div class="body"><?php echo $content; ?></div>
+      <div class="body"><?php echo $post['content']; ?></div>
     </div>
   </div>
   <div class="side">
-    <h2 class="h-title sub"><a href="/news">Latest News</a></h2>
+    <h2 class="h-title"><a href="/news">Other News</a></h2>
+    <ul class="post-list aside">
+      <?php foreach ($posts as $post): ?>
+        <li class="post">
+          <h4 class="title h-title sub"><a href="/news/<?php echo $post['url'] ?>"><?php echo $post['title']; ?></a></h4>
+          <div class="details"><time><?php $newDate = date("M-d-Y", strtotime($post['published_date'])); echo $newDate; ?></time></div>
+          <div class="body">
+            <p><?php echo $post['content_blurb']; ?> <a href="/news/<?php echo $post['url'] ?>" class="more">+</a></p>
+          </div>
+        </li>
+      <?php endforeach; ?>
+    </ul>
   </div>
 </div>
--- templates/news/list.php
+++ templates/news/list.php
@@ -16,7 +16,6 @@
     </ul>
   </div>
   <div class="side">
-    <h3 class="h-title sub"><a href="http://twitter.com/baralaye" target="_blank">Twitter</a></h3>
     <a class="twitter-timeline" href="https://twitter.com/baralaye" data-widget-id="391761770735353857" height="700">Tweets by @baralaye</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
   </div>
