073353a1aaab20408a415c217caf74c6323bb761
Ebi Baralaye <tech@baralaye.com>
2014-08-01 16:02:20 -0400

    updates to product list and detail views and data logic

--- templates/catalog/list.php
+++ templates/catalog/list.php
@@ -5,15 +5,19 @@
   </div>
   <ul class="product-list">
     <?php foreach ($products as $product): ?>
+      <?php 
+        if($product['name'] == null) {
+          $product['title_type'] = "id";
+          $product['title'] = $product['id'];
+        }
+        else {
+          $product['title_type'] = "name";
+          $product['title'] = $product['name'];
+        };
+      ?>
       <li class="item">
-        <div class="image"><a href="<?= $product['url']; ?>"><img src="/images/art/portfolio/small/<?= $product['image']; ?>.jpg" alt="<?= $product['name']; ?>" /></a></div>
-        <h4 class="title"><a href="<?= $product['url']; ?>">
-          <?php if ($product['name'] != null): ?>
-            <span class="name"><?= $product['name']; ?></span>
-          <?php else: ?>
-            <span class="id"><?= $product['id']; ?></span>
-          <?php endif; ?>
-        </a></h4>
+        <div class="image"><a href="<?= $product['url']; ?>"><img src="/images/art/portfolio/small/<?= $product['image']; ?>.jpg" alt="<?= $product['title']; ?>" /></a></div>
+        <h4 class="title <?= $product['title_type'] ?>"><a href="<?= $product['url']; ?>"><?= $product['title']; ?></a></h4>
       </li>
     <?php endforeach; ?>
   </ul>
