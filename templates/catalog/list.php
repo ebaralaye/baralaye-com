724bd5ded589e571965d0952c07d06d0bfc453e5
Ebi Baralaye <tech@baralaye.com>
2016-05-19 21:16:05 -0400

    updated list template to use ids for images

--- templates/catalog/list.php
+++ templates/catalog/list.php
@@ -6,7 +6,11 @@
   <ul class="item-list catalog">
     <?php foreach ($products as $product): ?>
       <li class="item">
-        <a href="<?= $product['url']; ?>" style="background-image:url(/images/art/portfolio/large/<?= $product['image']; ?>.jpg)" />
+        <?php if ($product['image']): ?>
+          <a href="<?= $product['url']; ?>" style="background-image:url(/images/art/portfolio/large/<?= $product['id']; ?>-<?= $product['image']; ?>.jpg)" />
+        <?php else: ?>
+          <a href="<?= $product['url']; ?>" style="background-image:url(/images/art/portfolio/large/<?= $product['id']; ?>.jpg)" />
+        <?php endif ?>
           <span class="title <?php if($product['title_type'] != null){ echo $product['title_type']; }; ?>"><?= $product['title']; ?></span>
         </a>
       </li>
