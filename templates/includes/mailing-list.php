1e944f7ca124d56d2a1a45f6ff0e5cae96e87364
Ebi Baralaye <tech@baralaye.com>
2017-06-09 19:03:57 -0700

    updated includes for social icons

--- templates/includes/mailing-list.php
+++ templates/includes/mailing-list.php
@@ -1,31 +1,32 @@
 <section id="mailing-list" class="mailing-list text-center clearfix">
-  <div id="mc_embed_signup">
-    <form class="form-container form-row" action="http://baralaye.us3.list-manage.com/subscribe/post?u=0497c3e7f0db060ff7eae50ba&amp;id=7522fe55b6" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-      <h1>Mailing List</h1>
-      <div class="row">
-        <div class="form-group mc-field-group col-xs-12 col-sm-4">
-          <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span></label>
-          <input name="EMAIL" id="mce-EMAIL" class="form-control required" type="email" value="" placeholder="Email Address *">
+  <div class="inner">
+    <div id="mc_embed_signup">
+      <form class="form-container form-row" action="http://baralaye.us3.list-manage.com/subscribe/post?u=0497c3e7f0db060ff7eae50ba&amp;id=7522fe55b6" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+        <div class="row">
+          <div class="form-group mc-field-group col-xs-8 col-sm-8">
+            <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span></label>
+            <input name="EMAIL" id="mce-EMAIL" class="form-control required" type="email" value="" placeholder="Email / Mailing List">
+          </div>
+          <!--<div class="form-group mc-field-group col-xs-6 col-md-3">
+            <label for="mce-FNAME">First Name </label>
+            <input name="FNAME"id="mce-FNAME" class="form-control" type="text" value="" placeholder="First Name">
+          </div>
+          <div class="form-group mc-field-group col-xs-6 col-md-3">
+            <label for="mce-LNAME">Last Name </label>
+            <input name="LNAME" id="mce-LNAME" class="form-control" type="text" value="" placeholder="Last Name">
+          </div>-->
+          <div class="form-foot col-xs-4 col-sm-4">
+            <input type="submit" value="Join" name="subscribe" id="mc-embedded-subscribe" class="btn submit f-l">
+          </div>
         </div>
-        <div class="form-group mc-field-group col-xs-6 col-sm-4">
-          <label for="mce-FNAME">First Name </label>
-          <input name="FNAME"id="mce-FNAME" class="form-control" type="text" value="" placeholder="First Name">
+        <div id="mce-responses" class="form-responses">
+          <div class="response error" id="mce-error-response" style="display:none"></div>
+          <div class="response error" id="mce-success-response" style="display:none"></div>
         </div>
-        <div class="form-group mc-field-group col-xs-6 col-sm-4">
-          <label for="mce-LNAME">Last Name </label>
-          <input name="LNAME" id="mce-LNAME" class="form-control" type="text" value="" placeholder="Last Name">
-        </div>
-      </div>
-      <div id="mce-responses" class="form-responses">
-        <div class="response error" id="mce-error-response" style="display:none"></div>
-        <div class="response error" id="mce-success-response" style="display:none"></div>
-      </div>
-      <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-      <div class="form-captcha"><input type="text" name="b_0497c3e7f0db060ff7eae50ba_7522fe55b6" tabindex="-1" value=""></div>
-      <div class="form-footer">
-        <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn submit">
-      </div>
-    </form>
+        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+        <div class="form-captcha"><input type="text" name="b_0497c3e7f0db060ff7eae50ba_7522fe55b6" tabindex="-1" value=""></div>
+      </form>
+    </div>
+    <script type="text/javascript" src="/libs/vendor/mailchimp/js/mailing-list.js"></script>
   </div>
-  <script type="text/javascript" src="/libs/vendor/mailchimp/js/mailing-list.js"></script>
 </section>
---
+++ templates/includes/social-icons.php
@@ -0,0 +1,13 @@
+<div class="social-icons">
+  <div class="inner">
+    <!--<a class="facebook" href="http://facebook.com/baralaye" target="_blank">Facebook</a>-->
+    <a class="instagram" href="http://instagram.com/baralaye" target="_blank">Instagram</a>
+    <a class="twitter" href="http://twitter.com/baralaye" target="_blank">Twitter</a>
+    <a class="linkedin" href="http://linkedin.com/in/ebaralaye" target="_blank">LinkedIn</a>
+    <!--<a href="http://pinterest.com/baralaye" target="_blank">Pinterest</a>-->
+    <!--<a href="http://baralaye.tumblr.com" target="_blank">Tumblr</a>-->
+    <!--<a href="https://plus.google.com/112097083655390633396/about" target="_blank">Google+</a>-->
+    <!--<a href="http://www.flickr.com/photos/baralaye/" target="_blank">Flickr</a>-->
+    <!--<a href="http://www.youtube.com/user/baralaye/videos?flow=grid&amp;view=1" target="_blank">YouTube</a>-->
+  </div>
+</div>
