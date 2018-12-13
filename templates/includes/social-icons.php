1f86aedaed8ae89bff19d38b07f923a64761f053
Ebi Baralaye <tech@baralaye.com>
2018-12-13 14:06:44 -0800

    updated styles for better call to action

--- templates/includes/social-icons.php
+++ templates/includes/social-icons.php
@@ -1,10 +1,10 @@
 <div class="social-icons">
   <div class="inner">
     <!--<a class="facebook" href="http://facebook.com/baralaye" target="_blank">Facebook</a>-->
-    <a class="instagram" href="http://instagram.com/baralaye" target="_blank">Instagram</a>
-    <a class="twitter" href="http://twitter.com/baralaye" target="_blank">Twitter</a>
+    <a class="btn instagram" href="http://instagram.com/baralaye" target="_blank">Instagram</a>
+    <a class="btn twitter" href="http://twitter.com/baralaye" target="_blank">Twitter</a>
     <!--<a class="linkedin" href="http://linkedin.com/in/ebaralaye" target="_blank">LinkedIn</a>-->
-    <a class="email" href="mailto:info@baralaye.com">INFO&#64;BARALAYE&#46;COM</a>
+    <a class="btn email" href="mailto:info@baralaye.com">INFO&#64;BARALAYE&#46;COM</a>
     <!--<a href="http://pinterest.com/baralaye" target="_blank">Pinterest</a>-->
     <!--<a href="http://baralaye.tumblr.com" target="_blank">Tumblr</a>-->
     <!--<a href="https://plus.google.com/112097083655390633396/about" target="_blank">Google+</a>-->
