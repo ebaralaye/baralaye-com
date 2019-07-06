b582a6fae2ecdbcf01a245e5fdaea8e8ecb2a748
Ebi Baralaye <tech@baralaye.com>
2019-07-06 08:59:24 -0400

    upadated home and pages template

--- templates/pages.php
+++ templates/pages.php
@@ -2,7 +2,7 @@
   <header>
     <h1><?= $name ?></h1>
     <?php if ($image_src): ?>
-      <h2><img src=<?= $image_src ?> alt=<? $image_alt ?> /></h2>
+      <h2><img src="<?= $image_src ?>" alt="<? $image_alt ?>" /></h2>
     <?php endif ?>
   </header>
   <main>
