e57f3a1e9ca24ca2bb94dddebefb727e61e2f0cc
Ebi Baralaye <tech@baralaye.com>
2020-12-31 11:29:05 -0500

    upated social icons again

--- templates/includes/social-icons.php
+++ templates/includes/social-icons.php
@@ -1,7 +1,7 @@
 <div class="social-icons">
   <div class="inner">
     <a class="btn instagram" href="http://instagram.com/baralaye" target="_blank">Instagram</a>
-    <a class="btn linkedin" href="http://linkedin.com/in/ebaralaye" target="_blank">LinkedIn</a>
+    <!--<a class="btn linkedin" href="http://linkedin.com/in/ebaralaye" target="_blank">LinkedIn</a>-->
     <!--<a class="btn email" href="mailto:info@baralaye.com">INFO&#64;BARALAYE&#46;COM</a>-->
     <!--<a href="http://pinterest.com/baralaye" target="_blank">Pinterest</a>-->
     <!--<a href="http://baralaye.tumblr.com" target="_blank">Tumblr</a>-->
@@ -9,6 +9,6 @@
     <a class="btn facebook" href="http://facebook.com/baralaye" target="_blank">Facebook</a>
     <a class="btn youtube" href="https://www.youtube.com/channel/UCSSMOL7YiDCuW9TTpoPvZ_Q" target="_blank">YouTube</a>
     <a class="btn twitter" href="http://twitter.com/baralaye" target="_blank">Twitter</a>
-    <a class="btn flickr" href="http://www.flickr.com/photos/baralaye/" target="_blank">Flickr</a>
+    <!--<a class="btn flickr" href="http://www.flickr.com/photos/baralaye/" target="_blank">Flickr</a>-->
   </div>
 </div>
